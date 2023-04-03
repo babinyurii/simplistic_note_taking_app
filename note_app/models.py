fd7eea319c8e22f43395c4ca17b2f15c2e50b4af
yuriy babin <babin.yurii@gmail.com>
2023-04-03 20:07:21 +0300

    user added to model

--- note_app/models.py
+++ note_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.timezone import now
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -8,6 +9,7 @@ class NoteModel(models.Model):
     date_created = models.DateTimeField(default=now())
     note_name = models.CharField(max_length=50, default="")
     note_text = models.TextField(max_length=500)
+    user = models.ForeignKey(User, default="", null=True, blank=True, on_delete=models.CASCADE)
     
     #urgency = models.Choices()
 
--- note_app/views.py
+++ note_app/views.py
@@ -2,24 +2,45 @@ from django.shortcuts import render, redirect
 from django.views.generic import ListView, CreateView, DetailView, DeleteView, UpdateView
 from .models import NoteModel
 from django.urls import reverse_lazy
+from django.http import HttpResponseRedirect
 
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 class NoteListView(ListView):
     model = NoteModel
-    queryset = NoteModel.objects.all().order_by("-date_created")
+    #queryset = NoteModel.objects.all().order_by("-date_created")
     paginate_by = 12
     template_name = "note_list.html"
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            return NoteModel.objects.order_by("-date_created").filter(user=self.request.user)
+        else:
+            return NoteModel.objects.none()
 
 
 class NoteCreateView(CreateView):
     model = NoteModel
     fields = ["note_name", "note_text"]
     template_name = "create_note.html"
-    success_url = reverse_lazy("note_list")
+    #success_url = reverse_lazy("note_list")
     #def get_success_url(self):
     #    return reverse_lazy("note_list")
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        if self.request.user:
+            self.object.user = self.request.user
+            self.object.save()
+        else:
+            self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        return reverse_lazy("note_list")
+
+
     
 class NoteDetailView(DetailView):
     template_name = "note_detail.html"
